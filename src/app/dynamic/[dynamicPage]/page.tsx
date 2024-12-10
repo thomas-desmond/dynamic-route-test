e7e6e4f236a535ca903d7cf80824fe07042efbb7
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-12-10 11:31:42 -0800

    initial commit

--- src/app/dynamic/[dynamicPage]/page.tsx
+++ src/app/dynamic/[dynamicPage]/page.tsx
@@ -1,3 +1,5 @@
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+
 export default async function Page({
     params,
   }: {
@@ -5,9 +7,23 @@ export default async function Page({
   }) {
     const page = (await params).dynamicPage;
 
+    const { results } = await (await getCloudflareContext()).env.DB.prepare(
+      "SELECT id, name, description, price, rating, imageUrl FROM products WHERE id = ?;"
+    )
+      .bind(1)
+      .run();
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1>Dynamic page {page}</h1>
+            {results.map((result: any) => (
+              <div key={result.id} className="product">
+                <h2>{result.name}</h2>
+                <p>{result.description}</p>
+                <p>Price: ${result.price}</p>
+                <p>Rating: {result.rating}</p>
+                <img src={result.imageUrl} alt={result.name} />
+              </div>
+            ))}
         </main>
     );
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 
-export default function Home() {
+export default async function Home() {
+
+  const { results: products } = await (await getCloudflareContext()).env.DB.prepare(
+    "SELECT id, name, description, price, imageUrl FROM Products"
+  ).run();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href="/dynamic/1">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Dynamic page
+         Go To Dynamic page
         </button>
       </Link>
+      <div className="mt-8">
+        {products.map((product: any) => (
+          <div key={product.id} className="mb-4">
+            <h2 className="text-xl font-bold">{product.name}</h2>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
